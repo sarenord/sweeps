896cb1b5fdffc63122f25d04e1c5c4dc6e896986
sarenord <ajsheld12@gmail.com>
2019-12-07 14:02:07 -0500

    merged back getjson function from branch web

--- src/main/resources/static/frontpage.js
+++ src/main/resources/static/frontpage.js
@@ -1,6 +1,24 @@
 const Http = new XMLHttpRequest();
 const baseUrl='localhost:8080/api/';
 
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+	  console.log("breakpoint");
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+
 function getMap() {
     var url = baseUrl + 'getMap/';
     Http.open("GET", "http://localhost:8080/api/getMap/");
